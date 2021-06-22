9030466500f8ece193925901393ed0d1439d417e
Brian Paez <brianpaezmarrero@gmail.com>
2021-06-22 14:13:41 -0700

    W1D2styling

---
+++ week_1/Algo/algo.js
@@ -0,0 +1,26 @@
+// Variabels
+var myName = "Brian Paez" //String 
+var age = '30' //String
+
+var myAge = 21 // Numbers or Integers
+var dead = false // Boolean
+var weight = 190.5// Floats
+
+// Arrays -- Ordered Data Structure
+var names = ["Brian","Jhon","Manuel"]
+
+// For Loops and While Loops 
+
+for(var num = 1; num < 100; num++){
+    console.log(num)
+}
+for(var nom = 1; nom < 7; nom+=2){
+    console.log(nom)
+}
+
+var nun = 1
+console.log(nun)
+nun++ // nun = nun + 1 or nun += 1
+console.log(nun) // 2
+nun++
+console.log(nun) // 3 
