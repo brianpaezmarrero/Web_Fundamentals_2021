5fe3b89a843d39355634d77fd3bb2b1984c88d7e
Brian Paez <brianpaezmarrero@gmail.com>
2021-06-24 12:58:53 -0700

    W1D4

---
+++ week_1/Algo/algo_d_4.js
@@ -0,0 +1,13 @@
+//funtion
+//defining funtion vs calling funtion
+//parameters vs arguments
+
+
+//defining 
+function double(someNumber){
+    var result = someNumber * 2
+    return result
+}
+
+//calling
+console.log(double(9))
